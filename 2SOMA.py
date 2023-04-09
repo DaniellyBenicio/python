733d292dcabfc688a298460fd65f19179c151ea2
DaniellyBenicio <115299113+DaniellyBenicio@users.noreply.github.com>
2023-04-09 17:58:22 -0300

    Update 2SOMA.py

--- 2SOMA.py
+++ 2SOMA.py
@@ -1,6 +1,7 @@
 #Crie um programa que leia dois números do usuário e exiba a soma desses números.
 
+print('Soma de dois números')
 n1 = int(input('Digite um número: '))
 n2 = int(input('Digite outro valor: '))
 soma = n1 + n2
-print('A soma entre os números', n1, ' + ', n2, ' = ', soma)
+print('A soma entre', n1, ' + ', n2, ' = ', soma)
