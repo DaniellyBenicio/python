240beae1adb335b49aa5b540d0b99c5ab605c2cf
DaniellyBenicio <115299113+DaniellyBenicio@users.noreply.github.com>
2023-04-09 18:55:09 -0300

    Update 9MENORNUMERO.py

--- 9MENORNUMERO.py
+++ 9MENORNUMERO.py
@@ -1,9 +1,9 @@
 #Crie um programa que leia uma lista de números do usuário e exiba o menor número dessa lista.
 print('Menor número!')
-menor = 100000000000000000
+menor = ''
 
 for i in range(1,6):
-    num = int(input('Informe um número: '))
-    if num < menor:
+    num = (input('Informe um número: '))
+    if len(num) > len(menor):
         menor = num
 print('Menor número: ', menor)
