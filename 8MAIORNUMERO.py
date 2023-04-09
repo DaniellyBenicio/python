75e8331b7d3a3d2381be77de7aeaff536fcaa4c8
DaniellyBenicio <115299113+DaniellyBenicio@users.noreply.github.com>
2023-04-09 19:01:32 -0300

    Update 8MAIORNUMERO.py

--- 8MAIORNUMERO.py
+++ 8MAIORNUMERO.py
@@ -1,10 +1,10 @@
 #Crie um programa que leia uma lista de números do usuário e exiba o maior número dessa lista.
 
 print('Maior número!')
-maior = 0
+maior = ''
 
 for i in range(1,6):
-    num = int(input('Informe um número: '))
-    if num > maior:
+    num = (input('Informe um número: '))
+    if len(num) > len(maior):
         maior = num
 print('Maior número: ', maior)
