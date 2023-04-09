c1c6139b8f28c26512de36918f988c415db9e84f
DaniellyBenicio <115299113+DaniellyBenicio@users.noreply.github.com>
2023-04-09 18:43:27 -0300

    Update 10MEDIA.py

--- 10MEDIA.py
+++ 10MEDIA.py
@@ -1,5 +1,5 @@
 #Crie um programa que leia uma lista de números do usuário e exiba a média desses números.
-
+print('MÉDIA')
 soma = 0
 
 for i in range(1,6):
@@ -7,4 +7,4 @@ for i in range(1,6):
     soma = soma + num
 media = soma/5
 
-print('A média entre os números foi: ', media)
+print('A média é: ', media)
