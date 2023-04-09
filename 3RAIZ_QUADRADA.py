a26cb0a2f7520111c3325677331a8982acbd8cb0
DaniellyBenicio <115299113+DaniellyBenicio@users.noreply.github.com>
2023-04-09 18:02:17 -0300

    Update 3RAIZ_QUADRADA.py

--- 3RAIZ_QUADRADA.py
+++ 3RAIZ_QUADRADA.py
@@ -1,5 +1,11 @@
 #Crie um programa que leia um número do usuário e exiba a sua raiz quadrada.
 from math import sqrt
+
+print('Calculando a raiz quadrada')
 num = int(input('Informe um número: '))
 
 print('A raiz quadrada é = ', sqrt(num))
+
+'''Outra forma de fazer sem usar o math, é utilizand o exponenciação.
+>> raizq = num**0.5 
+'''
